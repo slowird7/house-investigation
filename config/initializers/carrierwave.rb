4c42074453a1109e88aef828c3f70851ba2a1d9e
EC2 Default User <ec2-user@ip-172-31-95-173.ec2.internal>
2020-11-07 04:14:56 +0000

    update37

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,6 +7,7 @@ CarrierWave.configure do |config|
     config.storage :fog
     config.fog_provider = 'fog/aws'
     config.fog_directory  = 'house-investigation'
+    config.asset_host = 'https://s3-' + ENV['AWS_DEFAULT_REGION'] + '.amazonaws.com/house-investigation'
     config.fog_credentials = {
       provider: 'AWS',
       aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
@@ -19,5 +20,6 @@ CarrierWave.configure do |config|
     config.enable_processing = false if Rails.env.test?
   end
 end
- 
+
+# Carrierwave のファイル名に日本語を許可する
 CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
